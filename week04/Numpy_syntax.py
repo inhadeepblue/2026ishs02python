99b13295954073f0d4d03d85050cd7a406928abf
deepblue <deepblue@inha.ac.kr>
2026-09-08 20:30:57 +0900

    Numpy basics v0.3

--- week04/Numpy_syntax.py
+++ week04/Numpy_syntax.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-# numpy 배열 생성
 array01 = np.array([3, 2, 1])
 array02 = np.zeros((2, 3))
 array03 = np.ones((3, 2))
@@ -8,10 +7,10 @@ array04 = np.arange(1, 11, 2)
 # array04 = np.arange(5)
 array05 = np.linspace(0, 1, 5)
 
-print(array01)
-print(array02)
-array02[1, 1] = 9.0
-print(array02)
-print(array03)
-print(array04)
-print(array05)
+# 배열 속성
+print(array01.shape)  # 배열 모양 (행, 열)
+print(array02.shape)
+print(array02.ndim)  # 차원 수
+print(array03.dtype)  # 데이터 타입
+print(array04.size)  # 전체 원소 개수
+print(array05.size)
