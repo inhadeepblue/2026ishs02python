5dcbffdaa5f0b3a5210ae9383d5809fa49576cbc
deepblue <deepblue@inha.ac.kr>
2026-09-08 20:49:25 +0900

    Numpy basics v0.6

--- week04/Numpy_syntax.py
+++ week04/Numpy_syntax.py
@@ -1,12 +1,7 @@
 import numpy as np
 
-array01 = np.zeros((3, 4))
-array02 = np.array([[1, 2, 3, 88], [-9, 4, 5, 6], [7, 55, 8, 9]])
+array01 = np.array([[1, 3], [-9, 4], [55, 8]])
 
-array01[0, 0] = 7.0
 print(array01)
-#print(array01.reshape((2, 6)))
-print(array02)
-print(array01 + array02)
-print(array01 + 4.1)
-print(np.sum(array01))
+print(array01.T)
+print(array01.flatten())
