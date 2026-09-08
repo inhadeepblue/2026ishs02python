ca76b75028530cb638a58544b329d78ef64d6c8c
deepblue <deepblue@inha.ac.kr>
2026-09-08 20:20:28 +0900

    Numpy basics v0.1

---
+++ week04/Numpy_syntax.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# numpy 배열 생성
+np_array = np.array([3, '2', 1.7])
+print(np_array, type(np_array))
+
+# python 리스트 생성
+list_array = [3, '2', 1.7]
+print(list_array, type(list_array))
+
