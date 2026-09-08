e14296be188ef191e518ebc5c4b7d0c3f9151243
deepblue <deepblue@inha.ac.kr>
2026-09-08 21:39:48 +0900

    Pandas basics v0.8

--- week04/Pandas_syntax.py
+++ week04/Pandas_syntax.py
@@ -1,8 +1,19 @@
 import numpy as np
 import pandas as pd
 
-array = np.array([[4, 7, 10], [5, 8, 11], [6, 9, 12]])
-df = pd.DataFrame(array, columns=['a','b','c'], index=[1, 2, 3])
+array = np.array(
+    [
+        [100, 89, 100, 95],
+        [97, 99, 91, 96],
+        [99, 97, 82, 95]
+    ]
+)
+df = pd.DataFrame(array, columns=['Kor','Eng','Math','Cs'], index=[1, 2, 3])
+
+print(df.iat[2,1])
+print(df.at[3,'Eng'])
 print(df)
-print(df.melt().rename(columns={'variable':'var', 'value':'val'}).query('val <= 7').sort_values(by=['val'], ascending=False).head(3))
-# print(df.sort_values('mpg'))  # seaborn
+print(df.iloc[1:,2:])
+print(df.iloc[:,[0, 2]])
+print(df.loc[2:3, 'Math':'Cs'])  # print(df.loc[2:3,['Math', 'Cs']])
+print(df.loc[:,['Kor','Math']])
