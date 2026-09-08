b17a3fb354d034365cca8251cceca4d53b485e4d
deepblue <deepblue@inha.ac.kr>
2026-09-08 20:26:56 +0900

    Numpy basics v0.2

--- week04/Numpy_syntax.py
+++ week04/Numpy_syntax.py
@@ -1,10 +1,17 @@
 import numpy as np
 
 # numpy 배열 생성
-np_array = np.array([3, '2', 1.7])
-print(np_array, type(np_array))
+array01 = np.array([3, 2, 1])
+array02 = np.zeros((2, 3))
+array03 = np.ones((3, 2))
+array04 = np.arange(1, 11, 2)
+# array04 = np.arange(5)
+array05 = np.linspace(0, 1, 5)
 
-# python 리스트 생성
-list_array = [3, '2', 1.7]
-print(list_array, type(list_array))
-
+print(array01)
+print(array02)
+array02[1, 1] = 9.0
+print(array02)
+print(array03)
+print(array04)
+print(array05)
