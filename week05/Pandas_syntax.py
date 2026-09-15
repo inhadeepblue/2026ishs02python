2135b283b7e6cad37a4aa9a019321e5f1388a500
deepblue <deepblue@inha.ac.kr>
2026-09-15 19:39:01 +0900

    Pandas basics v1.0

--- week05/Pandas_syntax.py
+++ week05/Pandas_syntax.py
@@ -2,9 +2,7 @@ import numpy as np
 import pandas as pd
 
 df = pd.read_csv('bike.csv')
-#print(df.head())
-print(df.iloc[:5,0:5])
+print(df.iloc[5:8, [0, 5, 7]])
+print(df.loc[5:7, ['datetime', 'temp', 'humidity']])
 #print(df.info())
-df.iloc[2, 2] = np.nan
-print(df.iloc[:5,0:5])
-print(df.info())
+print(df[df['season']==4])  # 4/4 분기 전체 행
